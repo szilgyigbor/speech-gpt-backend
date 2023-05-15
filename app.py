e91a99350270f6069427e25598f30c35e00424eb
Gábor <szilgyigbor@gmail.com>
2023-05-15 11:47:37 +0200

    Transcribe_endpoint function is added to app.py.

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from speech_to_text import transcribe_audio
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return "hello"
+
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe_endpoint():
+    # Check if a file was received in the request
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file found'})
+
+    file = request.files['file']
+
+    transcription = transcribe_audio(file)
+
+    return jsonify(transcription)
 
 
 if __name__ == '__main__':
