b2b64dcf51539668e70dc0ed94172ae1b6ee6250
Gábor <szilgyigbor@gmail.com>
2023-05-16 10:46:24 +0200

    The incoming file is saved to use in API.

--- speech_to_text.py
+++ speech_to_text.py
@@ -6,12 +6,19 @@ openai.api_key = API_KEY
 
 
 def transcribe_audio(incoming_audio):
+    # Save the file first
+    file_path = "temp_audio.m4a"
+    incoming_audio.save(file_path)
+
     # Opening the file in binary
-    with open(incoming_audio, 'rb') as audio_file:
+    with open(file_path, 'rb') as audio_file:
         # Calling the OpenAI Speech to Text API
         response = openai.Audio.transcribe("whisper-1", audio_file)
 
     # Getting the text
     transcription = response['text']
+
+    # Delete the temporary file
+    os.remove(file_path)
 
     return transcription
