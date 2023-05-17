18fe905b6be9682247c189018b6fe7c9938cd736
Gábor <szilgyigbor@gmail.com>
2023-05-17 21:18:47 +0200

    Answer_endpoint is added to app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from speech_to_text import transcribe_audio
+from get_bot_answer import get_answer
 
 app = Flask(__name__)
 # CORS(app)   # Enables CORS for all routes
-CORS(app, resources={"/transcribe": {"origins": "http://localhost:4200"}})  # Enables CORS only for this route
+CORS(app, resources={"/api2/*": {"origins": "http://localhost:4200"}})  # Enables CORS only for this route
 
 
 @app.route('/')
@@ -12,7 +13,7 @@ def hello():
     return "hello"
 
 
-@app.route('/transcribe', methods=['POST'])
+@app.route('/api2/transcribe', methods=['POST'])
 def transcribe_endpoint():
     # Check if a file was received in the request
     if 'audioFile' not in request.files:
@@ -23,6 +24,14 @@ def transcribe_endpoint():
     transcription = transcribe_audio(file)
 
     return jsonify(transcription)
+
+
+@app.route('/api2/get-answer', methods=['POST'])
+def answer_endpoint():
+    message_history = request.get_json()
+    messages = get_answer(message_history)
+
+    return jsonify(messages)
 
 
 if __name__ == '__main__':
